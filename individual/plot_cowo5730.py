be6d424c741a44ed0a590eb913f55901a37fa08a
zzuxon <made4prezi@gmail.com>
2017-04-24 15:53:06 -0600

    Add my new exoplanet plot.

---
+++ individual/plot_cowo5730.py
@@ -0,0 +1,40 @@
+
+# basic imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load the data table (as defined in basics.py)
+from basics import data
+
+# MODIFY this to be your name!
+myname = "Cooper Wooten"
+
+# create a function definition
+def makeplot():
+
+    # MODIFY this to plot different columns!
+    # pull out columns from the data table
+    x = data['RA']
+    y = data['planet_semimajor']
+
+    # you may want to MODIFY this to make it prettier
+    # plot the data points
+    plt.scatter(x, y, alpha=0.5,
+                      marker='o',
+                      color='steelblue',
+                      edgecolor='none')
+
+    # you may need to MODIFY these limits to zoom in or out
+    # set the x and y limits to the min/max of the non-nan numbers
+    plt.xlim(np.nanmin(x), np.nanmax(x))
+    plt.ylim(np.nanmin(y), np.nanmax(y))
+
+    # you may need to MODIFY this
+    # make the scale be logarithmic or linear
+    plt.xscale('linear') # 'log' or 'linear' are possible x scales
+    plt.yscale('log') # 'log' or 'linear' are possible x scales
+
+    # add title and labels
+    plt.title("Could it be, the stupidest plot ever? Probably not.")
+    plt.xlabel('Right ascension of system in degrees')
+    plt.ylabel('Planet Semi-Major axis (AU)')
